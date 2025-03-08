f365f2e5d4d07f9f9ff205f11102d92c58efeedb
shalinis97 <shalinis1487@gmail.com>
2025-03-08 18:44:47 +0530

    regex for ga5 q1

--- app.py
+++ app.py
@@ -110,7 +110,10 @@ async def get_llm_prompt_for_yes() -> str:
 #-------- GA5 questions---------
 
 # ga5 q1 - Calculate total margin from Excel file
-@register_question(r".*(margin).*?(transactions).*?(before).*?(Theta).*?(?:IN|India).*")
+
+#@register_question(r".*(margin).*?(transactions).*?(before).*?(Theta).*?(?:IN|India).*")
+@register_question(r".*(margin).*?(transactions)?.*?(before).*?(Theta).*?(?:IN|India).*")
+
 
 
 async def calculate_total_margin(file: UploadFile) -> str:
